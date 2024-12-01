7a0ee0d9e2df869004b8f40acc451eb650122ca5
olivier <escoffier.olivier@laposte.net>
2024-12-01 11:48:35 +0100

    Formatage

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
 // Exemple d'un script pour un comportement simple
-document.addEventListener('DOMContentLoaded', function() {
-    // Par exemple, un message de bienvenue ou d'autres effets
+document.addEventListener("DOMContentLoaded", function () {
+  // Par exemple, un message de bienvenue ou d'autres effets
 });
